30284e2a224cbe0852dba1103556aae20d4f4845
Luis Andrey <luisandreyzuniga@gmail.com>
2022-09-18 17:17:19 -0600

    Register Info form almost done

--- EscuelaATI/src/app/modules/auth/register-user-info/register-user-info.component.ts
+++ EscuelaATI/src/app/modules/auth/register-user-info/register-user-info.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import{FormControl,FormGroup,Validators} from '@angular/forms'
 @Component({
   selector: 'app-register-user-info',
   templateUrl: './register-user-info.component.html',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterUserInfoComponent implements OnInit {
 
+  userInfoForm!:FormGroup;
   constructor() { }
 
   ngOnInit(): void {
+    this.userInfoForm=new FormGroup(
+      {
+        name:new FormControl('',[Validators.required]),
+        email:new FormControl('',[Validators.required,Validators.email]),
+        password:new FormControl('',[Validators.required,Validators.minLength(8)]),
+        class:new FormControl('',[])
+
+
+      }
+    )
   }
 
 }
--- EscuelaATI/src/app/modules/auth/register/register.component.ts
+++ EscuelaATI/src/app/modules/auth/register/register.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit ,ViewChild} from '@angular/core';
+import { MatStepper } from '@angular/material/stepper';
+import { RegisterUserInfoComponent } from '../register-user-info/register-user-info.component';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  @ViewChild('stepper',{static:true}) stepper:MatStepper;
+  @ViewChild('info',{static:true}) step1:RegisterUserInfoComponent;
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  stepOne(){
+    return this.step1.userInfoForm;
+  }
+  goForward(){
+    this.stepper.next()
+  }
+  goBack(){
+    this.stepper.previous()
+  }
+  isLast(){
+    return this.stepper.selectedIndex==this.stepper.steps.length-1;
+  }
+  isFirst(){
+    return this.stepper.selectedIndex==0;
+  }
+
+  
 
 }
+
