a8205b04fdbcb4cfab6d330c836a12a4d8c4d709
Luis Andrey <luisandreyzuniga@gmail.com>
2022-09-25 11:06:37 -0600

    [Update] adding routing to bottom clubs

--- EscuelaATI/src/app/modules/admin-view/admin-view-routing.module.ts
+++ EscuelaATI/src/app/modules/admin-view/admin-view-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BottomClubsComponent } from './bottom-clubs/bottom-clubs.component';
 
 import { TopClubsComponent } from './top-clubs/top-clubs.component';
 
 const routes: Routes = [
-  {path:'top',component:TopClubsComponent}
+  {path:'top',component:TopClubsComponent},
+  {path:'bottom',component:BottomClubsComponent}
 ];
 
 @NgModule({
