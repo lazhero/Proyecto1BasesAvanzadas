b5d7d5973cfb8491815fc8aeecc5f04edba44a35
Luis Andrey <luisandreyzuniga@gmail.com>
2022-09-25 09:15:24 -0600

    [Add] Adding admin view module

---
+++ EscuelaATI/src/app/modules/admin-view/admin-view.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminViewRoutingModule } from './admin-view-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AdminViewRoutingModule
+  ]
+})
+export class AdminViewModule { }
