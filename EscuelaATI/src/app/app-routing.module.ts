76cfa90c2d7c47e03352ff3cefefb6471a0e0902
Luis Andrey <luisandreyzuniga@gmail.com>
2022-09-26 19:39:59 -0600

    adding route

--- EscuelaATI/src/app/app-routing.module.ts
+++ EscuelaATI/src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path:'auth', loadChildren:()=>import('./modules/auth/auth.module').then(m=>m.AuthModule)},
   {path:'student',loadChildren:()=>import('./modules/student-view/student-view.module').then(m=>m.StudentViewModule)},
-  {path:'admin',loadChildren:()=>import('./modules/admin-view/admin-view.module').then(m=>m.AdminViewModule)}
+  {path:'admin',loadChildren:()=>import('./modules/admin-view/admin-view.module').then(m=>m.AdminViewModule)},
+  {path:'',redirectTo:'auth',pathMatch:'full'}
 ];
 
 @NgModule({
